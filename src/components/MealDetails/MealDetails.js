ce448f133211c67cd18bd879d36bfed89648beab
MD. ARIFUL HASAN TOTUL <mdahtotul@gmail.com>
2021-10-03 15:43:07 +0600

    done

--- src/components/MealDetails/MealDetails.js
+++ src/components/MealDetails/MealDetails.js
@@ -1,36 +1,52 @@
+import { Button } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 
 const MealDetails = () => {
    const [meal, setMeal] = useState({});
    const { mealId } = useParams();
+   const history = useHistory();
    useEffect(() => {
       fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
          .then((res) => res.json())
          .then((data) => setMeal(data.meals[0]));
    }, [mealId]);
 
+   const handleBack = () => {
+      history.push("/meals");
+   };
+
    return (
-      <div
-         style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            margin: "2rem 12%",
-            border: "1px solid goldenrod",
-            borderRadius: "10px",
-            overflow: "hidden",
-            background: "gray",
-            color: "#fff",
-         }}
-      >
-         <img style={{ width: "400px" }} src={meal.strMealThumb} alt="" />
-         <div style={{ margin: "0 4%" }}>
-            <h3>{meal.strMeal}</h3>
-            <small>{meal.strCategory}</small>
-            <p>{meal.strInstructions}</p>
+      <>
+         <div
+            style={{
+               display: "flex",
+               alignItems: "center",
+               justifyContent: "center",
+               margin: "2rem 12%",
+               border: "1px solid goldenrod",
+               borderRadius: "15px",
+               overflow: "hidden",
+               background: "gray",
+               color: "#fff",
+            }}
+         >
+            <img style={{ width: "400px" }} src={meal.strMealThumb} alt="" />
+            <div style={{ margin: "0 4%" }}>
+               <h3>{meal.strMeal}</h3>
+               <small>{meal.strCategory}</small>
+               <p>{meal.strInstructions?.slice(0, 1230)}</p>
+            </div>
          </div>
-      </div>
+
+         <Button
+            style={{ margin: " 0 12%" }}
+            onClick={handleBack}
+            variant="outlined"
+         >
+            Go back
+         </Button>
+      </>
    );
 };
 
